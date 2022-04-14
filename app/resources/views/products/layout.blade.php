325ef6c2ad6b3e54e31ae248304077400c3a640c
Joshua <jbcannon9@gmail.com>
2022-04-14 09:47:09 +0100

    my first commit

---
+++ app/resources/views/products/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>My Crud App</title>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/product.css') }}">
+</head>
+
+<body>
+
+  
+
+<div class="container">
+
+    @yield('content')
+
+</div>
+
